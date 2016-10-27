6800b3885c750d9764e6c9b1e492d76c0ac7eebb
a7b4e0 <a7b4e0@github.com>
2016-10-27 12:05:29 +1100

    Implement Regex matching in match.js

--- match.js
+++ match.js
@@ -1,5 +1,18 @@
 module.exports.match = function(hash) {
 
   // Write your solution to Task #2 - Match Metadata task here
-  
+  let regex = /card expire date (\w*)$/;
+  return matchHashTableKeys(hash, regex);
+}
+
+function matchHashTableKeys(hash, regex) {
+  var matchedKeys = [];
+
+  for (let key in hash) {
+    if (regex.test(key)) {
+      matchedKeys.push(key);
+    }
+  }
+
+  return matchedKeys;
 }
