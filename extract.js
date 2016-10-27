500b6c0f947725550911f25f8a161627283f9ad8
a7b4e0 <a7b4e0@github.com>
2016-10-27 12:50:35 +1100

    Finish extract.js

--- extract.js
+++ extract.js
@@ -1,5 +1,34 @@
-module.exports.extract = function(window) {
-  
-  // Write your solution to Task #2 - Extract Metadata task here
+var jsdom = require("jsdom");
 
+module.exports.extract = function(window) {
+
+  // Write your solution to Task #1 - Extract Metadata task here
+  let document = window.document;
+  let allInputs = document.getElementsByTagName("input");
+
+  var inputHashTable = {}
+  for (var i = 0; i < allInputs.length; i++) {
+    let input = allInputs[i];
+
+    let name = input.name;
+    let label = getLabelForInput(input);
+
+    if (label) {
+      let key = label;
+      let value = (label + " " + name);
+
+      inputHashTable[key] = value;
+    }
+  }
+
+  return inputHashTable;
+}
+
+function getLabelForInput(input) {
+  let inputCell = input.parentNode;
+  let inputRow = inputCell.parentNode;
+  let labelCell = inputRow.firstChild;
+  let label = labelCell.textContent;
+
+  return label.trim();
 }
